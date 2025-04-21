431e8512575d1cbfd8c9df1e218f6caa5acdc43f
hibisky <aito.kenza@gmail.com>
2025-04-21 22:13:00 +0400

    MAJ TODO

--- main.py
+++ main.py
@@ -68,26 +68,14 @@ if __name__ == "__main__":
     main()
     
 
-#TODO: algo/Généré les données en transit
-
-#TODO: system: ajout thread simulation/interface + deplacement robot
-
-#TODO: robot: afficher l'etage a lequel il pose le palet et il retire le palet
-#TODO: robot: ajout de trace pour comprendre deplacement hauteur
-#TODO: robot: resoudre pb initialisation et hauteur 
 #TODO: robot: Commentaires !
 
 #TODO: vision: retirer le bruit sur les images
-#TODO: vision: disier par 2 le compte pour avoir le bon nb de palet
 #TODO: vision: methode deconnexion camera 
     
 #TODO: interface: methode deconnexion interface
-#TODO: interface: ajouter la simulation à l'interface
-    
-#TODO: test: Imaginer les tests unitaires
-#TODO: test: Imaginer les tests integrations
+#TODO: interface: ajouter la simulation à l'interface NOT NECESSARY
 
-#TODO: DOC: Conception detaillée
 #TODO: DOC: Planning réel
 #TODO: DOC: Slides Soutenance
 #TODO: README: Update du readme 
